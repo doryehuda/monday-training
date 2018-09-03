b58f2839421dab0fc5314dc8cdfa8828a0d5b65e
Yehuda Dor <schiztech@gmail.com>
2018-09-03 11:53:15 +0300

    structured components tree and created item details form

--- ShoppingCartClient/src/App.js
+++ ShoppingCartClient/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ShoppingCart from "./containers/shopping-cart/shopping-cart";
-import ItemDetails from "./containers/item-details/item-details";
 
 class App extends Component {
   render() {
@@ -14,12 +13,7 @@ class App extends Component {
         </header>
 
         <div className="App-content">
-          <div className="App-shopping-cart">
-            <ShoppingCart  />
-          </div>
-          <div className="App-item-details">
-            <ItemDetails  />
-          </div>
+          <ShoppingCart />
         </div>
       </div>
     );
--- ShoppingCartClient/src/containers/shopping-cart/shopping-cart.js
+++ ShoppingCartClient/src/containers/shopping-cart/shopping-cart.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 import "./shopping-cart.css";
 import ItemsList from "../../components/items-list/items-list";
+import ItemDetails from "../../components/item-details/item-details";
 class ShoppingCart extends Component {
+  itemSubmitted(item){
+    console.log(item);
+  }
   render() {
     let items = [1, 2, 3, 4, 5, 6];
     return (
-      <div className="shopping-cart container">
-        <ItemsList items={items} />
+      <div className="Shopping-cart">
+        <div className="Shopping-cart-itemsList">
+          <ItemsList items={items} />
+        </div>
+        <div className="Shopping-cart-itemDetails">
+          <ItemDetails onSubmit={this.itemSubmitted} />
+        </div>
       </div>
     );
   }
